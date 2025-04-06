b376780be7671c23f17e32f0efb6e59a2cc56172
fd2955 <fd2955@gmail.com>
2025-04-06 17:54:05 +0300

    Yeni Sms-Otomasyon projesi dosyaları eklendi

---
+++ Sms-Otomasyon/SmsForm.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Sms_Otomasyon
+{
+    partial class SmsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumara = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMesaj = new System.Windows.Forms.TextBox();
+            this.btnGonder = new System.Windows.Forms.Button();
+            this.lblDurum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(189, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Numara";
+            // 
+            // txtNumara
+            // 
+            this.txtNumara.Location = new System.Drawing.Point(162, 84);
+            this.txtNumara.Name = "txtNumara";
+            this.txtNumara.Size = new System.Drawing.Size(128, 22);
+            this.txtNumara.TabIndex = 1;
+            this.txtNumara.Text = "+90";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(367, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Mesaj";
+            // 
+            // txtMesaj
+            // 
+            this.txtMesaj.Location = new System.Drawing.Point(344, 84);
+            this.txtMesaj.Name = "txtMesaj";
+            this.txtMesaj.Size = new System.Drawing.Size(100, 22);
+            this.txtMesaj.TabIndex = 3;
+            // 
+            // btnGonder
+            // 
+            this.btnGonder.Location = new System.Drawing.Point(162, 191);
+            this.btnGonder.Name = "btnGonder";
+            this.btnGonder.Size = new System.Drawing.Size(75, 23);
+            this.btnGonder.TabIndex = 4;
+            this.btnGonder.Text = "Gönder";
+            this.btnGonder.UseVisualStyleBackColor = true;
+            this.btnGonder.Click += new System.EventHandler(this.btnGonder_Click);
+            // 
+            // lblDurum
+            // 
+            this.lblDurum.AutoSize = true;
+            this.lblDurum.Location = new System.Drawing.Point(229, 257);
+            this.lblDurum.Name = "lblDurum";
+            this.lblDurum.Size = new System.Drawing.Size(44, 16);
+            this.lblDurum.TabIndex = 5;
+            this.lblDurum.Text = "label3";
+            this.lblDurum.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // SmsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblDurum);
+            this.Controls.Add(this.btnGonder);
+            this.Controls.Add(this.txtMesaj);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNumara);
+            this.Controls.Add(this.label1);
+            this.Name = "SmsForm";
+            this.Text = "SmsForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumara;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMesaj;
+        private System.Windows.Forms.Button btnGonder;
+        private System.Windows.Forms.Label lblDurum;
+    }
+}
---
+++ Sms-Otomasyon/SmsForm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace Sms_Otomasyon
+{
+    public partial class SmsForm : Form
+    {
+        public SmsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGonder_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Twilio bilgileriniz (bunları kendi bilgilerinle doldur)
+                const string accountSid = "ACaaffd159c226bb3d01f48e8b042962a7";
+                const string apiKeySid = "SK663688821d14b2a7c74614556a8b7615";
+                const string apiSecret = "CapU32Ath9NTyrVnvCF6rZMsEYDRpbDK";
+                const string fromPhone = "+12705618169"; // Twilio'dan aldığın numara
+
+                Twilio.TwilioClient.Init(apiKeySid, apiSecret, accountSid);
+
+                var message = Twilio.Rest.Api.V2010.Account.MessageResource.Create(
+                    to: new Twilio.Types.PhoneNumber(txtNumara.Text),
+                    from: new Twilio.Types.PhoneNumber(fromPhone),
+                    body: txtMesaj.Text
+                );
+
+                lblDurum.Text = "✅ Mesaj gönderildi! SID: " + message.Sid;
+            }
+            catch (Exception ex)
+            {
+                lblDurum.Text = "❌ Hata: " + ex.Message;
+                Console.WriteLine(ex);
+            }
+
+        }
+    }
+}
